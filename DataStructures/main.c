46ee61b640713e25ce10d589af8b37f0fadcb03c
BryantDavidDavis <davis.bryant@gmail.com>
2014-10-10 11:22:56 -0700

    Initial Commit

---
+++ DataStructures/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStructures
+//
+//  Created by Bryant Davis on 10/10/14.
+//  Copyright (c) 2014 Bryant Davis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
